2690f463b6a82cb297aaee8e7552395fb4499785
Max Gilbreath <maxgilbreath@gmail.com>
2021-11-05 13:44:09 -0500

    Added Better Display Names

--- MyBackPain/MyBackPain/Models/BackPain.cs
+++ MyBackPain/MyBackPain/Models/BackPain.cs
@@ -13,6 +13,8 @@ namespace MyBackPain.Models
         public int ID { get; set; }
         [StringLength(60, MinimumLength = 1)]
         [Required]
+        [Display(Name = "Do I Have Pack Pain")]
+
         public string DoIHaveBackPain { get; set; }
         [Display(Name = "Date")]
 
@@ -20,12 +22,18 @@ namespace MyBackPain.Models
         public DateTime Day { get; set; }
         [StringLength(60, MinimumLength = 3)]
         [Required]
+        [Display(Name = "Is It My Fault")]
+
         public string IsItMyFault { get; set; }
         [StringLength(60, MinimumLength = 1)]
         [Required]
+        [Display(Name = "Did I Take Ibuprofen")]
+
         public string DidITakeIbuprofen { get; set; }
         [StringLength(60, MinimumLength = 1)]
         [Required]
+        [Display(Name = "Do I Shake My fists At The Sky")]
+
         public string DoIShakeMyFistsAtTheSky { get; set; }
     }
 }
