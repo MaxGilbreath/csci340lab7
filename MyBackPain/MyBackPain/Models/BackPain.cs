c49adda4d0f9c1893ddf185f450a2618bd9d90ac
Max Gilbreath <maxgilbreath@gmail.com>
2021-11-05 00:07:44 -0500

    Search and Validation Added

--- MyBackPain/MyBackPain/Models/BackPain.cs
+++ MyBackPain/MyBackPain/Models/BackPain.cs
@@ -11,13 +11,21 @@ namespace MyBackPain.Models
     public class BackPain
     {
         public int ID { get; set; }
+        [StringLength(60, MinimumLength = 1)]
+        [Required]
         public string DoIHaveBackPain { get; set; }
         [Display(Name = "Date")]
 
         [DataType(DataType.Date)]
         public DateTime Day { get; set; }
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string IsItMyFault { get; set; }
+        [StringLength(60, MinimumLength = 1)]
+        [Required]
         public string DidITakeIbuprofen { get; set; }
+        [StringLength(60, MinimumLength = 1)]
+        [Required]
         public string DoIShakeMyFistsAtTheSky { get; set; }
     }
 }
--- MyBackPain/MyBackPain/Pages/BackPains/Index.cshtml.cs
+++ MyBackPain/MyBackPain/Pages/BackPains/Index.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using MyBackPain.Data;
 using MyBackPain.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace MyBackPain.Pages.BackPains
 {
@@ -20,9 +21,19 @@ namespace MyBackPain.Pages.BackPains
         }
 
         public IList<BackPain> BackPain { get;set; }
-
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+        public SelectList Looks { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string DoIShakeMyFistsAtTheSky { get; set; }
         public async Task OnGetAsync()
         {
+            var pains = from m in _context.BackPain
+                         select m;
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                pains = pains.Where(s => s.DoIHaveBackPain.Contains(SearchString));
+            }
             BackPain = await _context.BackPain.ToListAsync();
         }
     }
